d2a860a8f10f3eecd31f18e3e01744103c1111d3
yasmeen10 <yasmeenwaleed844@gmail.com>
2024-03-15 01:08:30 +0200

    newUpdate

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-require('./db');
-require('mongoose')
-const cors = require('cors');
+require("dotenv").config();
+require("./db");
+require("mongoose");
+const cors = require("cors");
 
 const express = require("express");
 
@@ -17,48 +17,48 @@ const userRouter = require("./routes/user.router");
 
 const profileRouter = require("./routes/profile.router");
 
-const productRouter = require('./routes/product.router');
+const productRouter = require("./routes/product.router");
 
 const stripeRouter = require("./routes/stripe.router");
 
-const adminRouter = require('./routes/admin.router');
+const adminRouter = require("./routes/admin.router");
 
-const orderRouter = require('./routes/order.router');
+const orderRouter = require("./routes/order.router");
 
-const cartRouter = require('./routes/cart.router');
+const cartRouter = require("./routes/cart.router");
 
-const categoryRouter = require('./routes/category.router');
+const categoryRouter = require("./routes/category.router");
 
-const reviewRouter = require('./routes/review.router');
+const reviewRouter = require("./routes/review.router");
 
-const ratingRouter = require('./routes/rating.router');
+const ratingRouter = require("./routes/rating.router");
 
-const auth = require('./middleware/auth.middleware');
+const auth = require("./middleware/auth.middleware");
 
-const admin = require('./middleware/admin.middleware');
+const admin = require("./middleware/admin.middleware");
 
 app.use("/api/v1/users", userRouter);
 
-app.use('/api/v1/products', productRouter);
+app.use("/api/v1/products", productRouter);
 
-app.use(auth);
+//app.use(auth);
 
-app.use('/api/v1/profile', profileRouter);
+app.use("/api/v1/profile", profileRouter);
 
-app.use('/api/v1/categories', categoryRouter);
+app.use("/api/v1/categories", categoryRouter);
 
 app.use("/api/v1/cart/", cartRouter);
 
-app.use('/api/v1/orders/', orderRouter);
+app.use("/api/v1/orders/", orderRouter);
 
-app.use('/api/v1/stripe', stripeRouter);
+app.use("/api/v1/stripe", stripeRouter);
 
-app.use('/api/v1/products', reviewRouter, ratingRouter);
+app.use("/api/v1/products", reviewRouter, ratingRouter);
 
-app.use(admin);
+//app.use(admin);
 
-app.use('/api/v1/admin/', adminRouter);
+app.use("/api/v1/admin/", adminRouter);
 
 app.listen(port, () => {
-    console.log(`Connected on port ${port}...`);
+  console.log(`Connected on port ${port}...`);
 });
