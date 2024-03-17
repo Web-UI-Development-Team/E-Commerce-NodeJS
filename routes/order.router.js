c3c32c6534e0f9b3acb846bcfcb485ff5cdfe566
esraaali138 <esraaali1999138@gmail.com>
2024-03-17 14:00:09 -0700

    added

---
+++ routes/order.router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const controllers = require('../controllers/order.controller');
+const admin = require('../middleware/admin.middleware');
+
+router.get('/', controllers.getAllOrders);
+
+router.get('/:id', controllers.getSpecificOrder);
+
+router.patch('/:id/cancel', controllers.cancelOrder);
+
+router.post('/', controllers.createNewOrder);
+
+module.exports = router;
